e9799800cf98a7f774e11a8fffbe7b4aefe04d7f
DavidChagas <36827231+DavidChagas@users.noreply.github.com>
2020-01-19 07:55:58 -0300

    desenvolvimento front

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'; // necessário a importação sempre que o arquivo utilizar js + html
+import ReactDOM from 'react-dom'; //possibilita a comunicação do react com o html (no caso de projeto web)
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
